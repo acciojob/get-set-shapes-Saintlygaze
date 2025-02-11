0106cf794e914b3119e1c5bf79bd8c388b9f26e4
Saintlygaze <153154293+Saintlygaze@users.noreply.github.com>
2025-02-11 13:06:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this._width = width;
+		this._height = height;
+	}
 
-class Square extends Animal {}
+	getArea(){
+		console.log(this._width*this._height);
+	}
+
+	get width(){
+		return this._width;
+	}
+	get height(){
+		return this._height;
+	}
+}
+
+class Square extends Rectangle {
+	constructor(side)
+	{
+		super(side,side);
+		
+	}
+	getPerimeter()
+	{
+		console.log(4*side);
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
